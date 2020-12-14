fb1bb9f2bb65ef8d83e771ef5a9fb8e1ab2800e7
Charles Meng <44573025+KratosMC@users.noreply.github.com>
2020-12-14 18:47:58 +0800

    Add UI and server file

---
+++ shinyapp/server.R
@@ -0,0 +1,53 @@
+# Load the packages
+
+library(shiny)
+library(tidyverse)
+library(tidyquant)
+
+shinyServer(function(input, output) {
+    
+    stock_price <- read_csv("stock.csv")
+    stock_price$date=as.Date(stock_price$date)
+    stock_return <- read_csv("stock_return.csv")
+    stock_return$date <- as.Date(stock_return$date)
+    
+    output$stock <- DT::renderDataTable({
+        DT::datatable(stock_price, options = list(scrollX =TRUE, pageLength = 20))
+    })
+    
+    observe({
+        output$plot <- NULL
+        output$warning <- NULL
+        req(input$initPos, input$gild, input$pfe, input$bntx)
+        if (sum(input$gild, input$pfe, input$bntx) != 1) {
+            output$warning <- renderText(HTML("Warning: Total weight must equal to 1."))
+        } else {
+            initial_position <- input$initPos
+            wts <- c(input$gild, input$pfe, input$bntx)
+            
+            portfolio_growth <- stock_return %>%
+                tq_portfolio(assets_col   = symbol, 
+                             returns_col  = monthly.return, 
+                             weights      = wts, 
+                             col_rename   = "investment.growth",
+                             wealth.index = TRUE) %>%
+                mutate(investment.growth = investment.growth * initial_position)
+            
+            gg <- portfolio_growth %>%
+                ggplot(aes(x = date, y = investment.growth)) +
+                geom_line(size = 2, color = palette_light()[[5]]) +
+                labs(title = "Portfolio Performance",
+                     subtitle = paste0("with ", input$gild * 100, "% GILD,", input$pfe * 100, "% PFE,",
+                                       input$bntx * 100, "% BNTX"),
+                     x = "", y = "Portfolio Value") +
+                geom_smooth(method = "loess") +
+                geom_text(aes(label = round(investment.growth)),nudge_x=0.1,nudge_y=0.1)+
+                theme_tq() +
+                scale_color_tq() +
+                scale_y_continuous(labels = scales::dollar)
+            
+            output$plot <- renderPlot({print(gg)})
+        }
+    })
+    
+})
---
+++ shinyapp/ui.R
@@ -0,0 +1,56 @@
+# Load the packages
+library(shiny)
+library(shinydashboard)
+
+shinyUI(dashboardPage(
+    dashboardHeader(title = "Portfolio Analysis on Healthcare Stocks", titleWidth = 400),
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem("Stock Price", tabName = "stock_price",icon=icon("comments-dollar")),
+            menuItem("Portfolio Performance", tabName = "performance",icon = icon("chart-line"))
+        )
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "stock_price",
+                    
+                    title = "Stock Price",
+                    status = "primary",
+                    collapsible = FALSE,
+                    solidHeader = TRUE,
+                    width = 12,
+                    DT::dataTableOutput("stock")
+            ),
+            tabItem(tabName = "performance",
+                    
+                    title = "Portfolio Performance",
+                    status = "primary",
+                    collapsible = FALSE,
+                    solidHeader = TRUE,
+                    width = 12,
+                    fluidRow(
+                        column(
+                            4,
+                            tags$h3("Initial Position in $"),
+                            numericInput("initPos", NULL, 250000, 20000, 1000000, 5000)
+                        ),
+                        column(
+                            8,
+                            tags$h3("Weight of Individual Stock"),
+                            fluidRow(
+                                column(2, tags$h4("GILD:")),
+                                column(2, numericInput("gild", NULL, 0.1, 0, 1, step = 0.05)),
+                                column(2, tags$h4("PFE:")),
+                                column(2, numericInput("pfe", NULL, 0.4, 0, 1, step = 0.05)),
+                                column(2, tags$h4("BNTX:")),
+                                column(2, numericInput("bntx", NULL, 0.5, 0, 1, step = 0.05))
+                            ),
+                            tags$div(style = "color:red;",
+                                     textOutput("warning"))
+                            
+                        )
+                    ),
+                    plotOutput("plot"))
+        )
+    )
+))
